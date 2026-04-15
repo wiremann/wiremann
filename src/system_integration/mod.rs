a1a222aa9e28564a1a738660d867479810170ba8
Anant Narayan <anantnrg@gmail.com>
2026-04-15 13:48:48 +0530

    create media controls

--- src/system_integration/mod.rs
+++ src/system_integration/mod.rs
@@ -1,6 +1,7 @@
 use crate::controller::{commands::SystemIntegrationCommand, events::SystemIntegrationEvent};
 use crossbeam_channel::{Receiver, Sender};
-use souvlaki::MediaControls;
+use raw_window_handle::RawWindowHandle;
+use souvlaki::{MediaControls, PlatformConfig};
 
 pub struct SystemIntegration {
     pub tx: Sender<SystemIntegrationEvent>,
@@ -10,13 +11,33 @@ pub struct SystemIntegration {
 }
 
 impl SystemIntegration {
-    pub fn new() -> (
+    pub fn new(raw_window_handle: Option<RawWindowHandle>) -> (
         Self,
         Sender<SystemIntegrationCommand>,
         Receiver<SystemIntegrationEvent>,
     ) {
         let (cmd_tx, cmd_rx) = crossbeam_channel::unbounded();
         let (event_tx, event_rx) = crossbeam_channel::unbounded();
+
+        #[cfg(not(target_os = "windows"))]
+        let hwnd = None;
+
+        #[cfg(target_os = "windows")]
+        let hwnd = raw_window_handle.and_then(|handle| {
+            let handle = match handle {
+                RawWindowHandle::Win32(h) => h,
+                _ => unreachable!()
+            };
+            Some(handle.hwnd)
+        });
+
+        let config = PlatformConfig {
+            hwnd,
+            dbus_name: "app.wiremann.wiremann",
+            display_name: "Wiremann"
+        };
+
+        let controls = MediaControls::new(config).ok();
 
         (
             Self {
@@ -27,5 +48,10 @@ impl SystemIntegration {
             cmd_tx,
             event_rx,
         )
+    }
+
+    pub fn run(self) {
+
+        let (souvlaki_tx, souvlaki_rx) = crossbeam_channel::unbounded();
     }
 }
