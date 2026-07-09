a033e2b683786bc9ed262e341d1d87105149d3a2
Anant Narayan <anantnrg@gmail.com>
2026-07-09 19:48:16 +0530

    add tracks table header

--- src/ui/pages/library/sections/tracks.rs
+++ src/ui/pages/library/sections/tracks.rs
@@ -31,6 +31,63 @@ impl Render for TracksSection {
                                 .mt_1()
                                 .bg(theme.library_tracks_section_title),
                         ),
+                )
+                .child(
+                    div()
+                        .h_16()
+                        .w_full()
+                        .flex()
+                        .items_center()
+                        .text_xs()
+                        .font_weight(FontWeight::NORMAL)
+                        .text_color(theme.library_table_header_text)
+                        .border_b_1()
+                        .border_color(theme.library_table_border)
+                        .child(
+                            div()
+                                .w_20()
+                                .h_full()
+                                .flex()
+                                .items_center()
+                                .justify_center()
+                                .child("#"),
+                        )
+                        .child(
+                            div()
+                                .w_3_5()
+                                .h_full()
+                                .flex()
+                                .items_center()
+                                .justify_center()
+                                .child("TITLE"),
+                        )
+                        .child(
+                            div()
+                                .w_1_2()
+                                .h_full()
+                                .flex()
+                                .items_center()
+                                .justify_center()
+                                .child("ARTIST"),
+                        )
+                        .child(
+                            div()
+                                .w_1_2()
+                                .h_full()
+                                .flex()
+                                .items_center()
+                                .justify_center()
+                                .child("ALBUM"),
+                        )
+                        .child(
+                            div()
+                                .w_24()
+                                .h_full()
+                                .flex()
+                                .items_center()
+                                .justify_center()
+                                .child("DURATION"),
+                        ),
                 ),
         )
     }
